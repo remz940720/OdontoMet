fcf816b614c9f61ca77d166043840498a8bc22bc
Eduardo <remz940720@gmail.com>
2019-11-08 15:01:15 -0600

    Prueba

--- MVC/index.php
+++ MVC/index.php
@@ -23,7 +23,15 @@
 		<label class="formulario__label">Contraseña</label>
 		<input type="submit" class="submit" value="Iniciar Sesión">
 	</form>
-	
+	<form action="#" class="formulario">
+		<h1 class="titulo">INICIAR SESIÓN</h1>
+		<input type="text" class="formulario__input">
+		<label class="formulario__label">Usuario</label>
+		<input type="password" class="formulario__input">
+		<label class="formulario__label">Contraseña</label>
+		<input type="submit" class="submit" value="Iniciar Sesión">
+	</form>
+
 	<script src="js/formLog.js"></script>
 	<br>
 	<footer> Información del dentista y del consultorio</footer>
